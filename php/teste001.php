7bca5a4038a6d2aff178b064dc8e60942abdba64
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-16 11:33:56 -0300

    add php > #001 - Cria uma soma de 2 números e mostre o resultado:

---
+++ php/teste001.php
@@ -0,0 +1,20 @@
+<?php
+
+#001 - Cria uma soma de 2 números e mostre o resultado:
+
+$n1 = 10;
+
+$n2 = 3;
+
+$total = $n1+$n2;
+
+echo($total);
+
+#Executado em 0,051 seg
+
+/*
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+PHP 8.2.20 (cli) (built: Jun 17 2024 13:33:14) (NTS)
+*/
