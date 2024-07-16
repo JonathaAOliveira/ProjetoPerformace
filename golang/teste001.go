4861aa317679d0ccf9f4ab645f3ea3560806f3fe
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-16 15:29:51 -0300

    add GO > #001 - Cria uma soma de 2 números e mostre o resultado (0.16)

---
+++ golang/teste001.go
@@ -0,0 +1,22 @@
+/*001 - Cria uma soma de 2 números e mostre o resultado:*/
+package main
+
+import "fmt"
+
+func main() {
+	var n1 = 10;
+	var n2 = 3;
+
+	var total = n1 + n2;
+
+    fmt.Println("Total", total)
+}
+
+/*Executado em 0,16 seg*/
+
+/*
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+go version go1.19.8 linux/amd64
+*/
