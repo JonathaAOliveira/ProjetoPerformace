234f72631d7bb7d99b572aaa6ddab5547885d0ca
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-16 14:16:45 -0300

    add JS > 001 - Cria uma soma de 2 números e mostre o resultado

---
+++ js/teste001.js
@@ -0,0 +1,18 @@
+/*001 - Cria uma soma de 2 números e mostre o resultado:*/
+
+let n1 = 10;
+
+let n2 = 3;
+
+let total = n1+n2;
+
+console.log(total);
+
+/*Executado em 0,060 seg*/
+
+/*
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+node v20.15.1 / npm 10.7.0
+*/
