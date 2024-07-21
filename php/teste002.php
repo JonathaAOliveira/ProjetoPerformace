96df32a42694bc81dc4834d6b70c4cd5b2af7ff1
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-21 11:35:26 -0300

    add php > #002 - Crie um sistema de cadastro simples > 0.023

---
+++ php/teste002.php
@@ -0,0 +1,21 @@
+<?php
+#002 - Crie um sistema de cadastro simples
+
+$nome = "jonatha";
+$idade = 31;
+$email = "jonatha.araujo.oliveira@gmail.com";
+
+echo("\n Nome: $nome");
+echo("\n Idade: $idade");
+echo("\n E-mail: $email");
+
+#Executado em 0,023 seg
+
+/*
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+PHP 8.2.20 (cli) (built: Jun 17 2024 13:33:14) (NTS)
+*/
+
+?>
