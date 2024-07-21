2808c56e518d6355480062a0a18299ced55ecf1a
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-21 15:18:57 -0300

    add python >#003 - Crie um programa de média escolar>0.021s

---
+++ python/teste003.py
@@ -0,0 +1,26 @@
+#003 - Crie um programa de média escolar
+
+nome = "jonatha"
+
+n1 = 8.9
+n2 = 7.8
+n3 = 6.7
+n4 = 5.6
+
+media = ((n1+n2+n3+n4)/4)
+
+print("O Aluno: ", nome, " possui a média: ", media, "\n")
+
+if(media >= 7.0):
+    print("Aprovado")
+else: 
+    print("Reprovado")
+
+#Executado em 0,021 seg
+
+"""
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+Python 3.11.2 (main, May  2 2024, 11:59:08) [GCC 12.2.0]
+"""
