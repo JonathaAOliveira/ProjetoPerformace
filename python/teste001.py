ab9ad98c88895f1aae6c60732a8790da7f1cf828
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-21 10:14:56 -0300

    add python > #001 - Cria uma soma de 2 números e mostre o resultado - 0.028s

---
+++ python/teste001.py
@@ -0,0 +1,18 @@
+#001 - Cria uma soma de 2 números e mostre o resultado:
+n1 = 10
+
+n2 = 3
+
+total = n1+n2
+
+print(total)
+
+
+#Executado em 0,028 seg
+
+"""
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+Python 3.11.2 (main, May  2 2024, 11:59:08) [GCC 12.2.0]
+"""
