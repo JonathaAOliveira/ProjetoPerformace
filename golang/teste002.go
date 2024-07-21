7bafd24e8e38739f635b4f453da8a3c59352fa48
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-21 11:29:35 -0300

    add go >#002 - Crie um sistema de cadastro simples > 0.15s

---
+++ golang/teste002.go
@@ -0,0 +1,26 @@
+/*002 - Crie um sistema de cadastro simples:*/
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome = "Jonatha";
+	var idade = 31;
+	var email = "jonatha.araujo.oliveira@gmail.com";
+
+	fmt.Println("Nome: ", nome);
+	fmt.Println("Idade ", idade);
+	fmt.Println("email ", email);
+
+}
+
+
+/*Executado em 0,15 seg*/
+
+/*
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+go version go1.19.8 linux/amd64
+*/
