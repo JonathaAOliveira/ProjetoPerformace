c7b99d60ac2f0b0bf960ecf62a2a3aec71fba780
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-21 11:21:42 -0300

    add python >#002 - Crie um sistema de cadastro simples - 0.057s

---
+++ python/teste002.py
@@ -0,0 +1,20 @@
+#002 - Crie um sistema de cadastro simples:
+
+nome = "Jonatha"
+idade = 31
+email = "jonatha.araujo.oliveira@gmail.com"
+
+print("\n Dados recebidos: \n")
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"E-mail: {email}")
+
+#Executado em 0,057 seg
+
+"""
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+Python 3.11.2 (main, May  2 2024, 11:59:08) [GCC 12.2.0]
+"""
