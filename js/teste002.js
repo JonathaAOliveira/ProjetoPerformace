1021900475d32d723fbf06ed11cede3fb9067d00
JonathaAOliveira <jonatha.araujo.oliveira@gmail.com>
2024-07-21 11:40:55 -0300

    add js >#002 - Crie um sistema de cadastro simples > 0.057

---
+++ js/teste002.js
@@ -0,0 +1,18 @@
+/*002 - Crie um sistema de cadastro simples*/
+
+nome = "jonatha"
+idade = 31
+email = "jonatha.araujo.oliveira@gmail.com"
+
+console.log("Nome: ", nome)
+console.log("Idade: ", idade)
+console.log("E-mail: ", email)
+
+/*Executado em 0,053 seg*/
+
+/*
+OS: Debian GNU/Linux 12 (bookworm) x86_64 
+CPU: Intel i5-6200U (4) @ 2.800GHz
+Memory: 4049MiB / 11867MiB
+node v20.15.1 / npm 10.7.0
+*/
